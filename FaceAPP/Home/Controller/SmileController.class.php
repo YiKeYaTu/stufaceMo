3eeb774810c84532b2aa413cd79713ff62d960f7
duwonders <duwonders@gmail.com>
2016-08-07 16:36:11 +0800

    dsa

--- FaceAPP/Home/Controller/SmileController.class.php
+++ FaceAPP/Home/Controller/SmileController.class.php
@@ -74,7 +74,7 @@ class SmileController extends Controller {
         if($time <= 12){
             $stuId = session('uid');
             if(stuId == null){
-                $this->ajaxReturn(2);
+                $this->ajaxReturn(10);
             }
         	$picUid = I('post.uid');
             echo $stuId."   "$picUid;
