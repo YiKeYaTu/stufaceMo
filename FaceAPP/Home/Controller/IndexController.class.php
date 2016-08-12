900d876fdb0cae47c0e493ab4ac9d705009da75d
YiKeYaTu <liangchen005005@163.com>
2016-08-12 10:19:40 +0800

    asas

--- FaceAPP/Home/Controller/IndexController.class.php
+++ FaceAPP/Home/Controller/IndexController.class.php
@@ -57,6 +57,7 @@ class IndexController extends Controller {
         );
         $data = $this->curl_api('http://hongyan.cqupt.edu.cn/MagicLoop/index.php?s=/addon/Api/Api/apiJsTicket', $conf);
         $jsapi = "jsapi_ticket=".$data['data']."&noncestr=$nonceStr"."&timestamp=$timestamp"."&url=".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+        var_dump($jsapi);
         $jsapi_tickit = sha1($jsapi);
         return array(
             'appId' => $appid, // 必填，公众号的唯一标识
