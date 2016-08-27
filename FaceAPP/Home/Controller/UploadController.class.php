57ecb33af07630775c602c138863b749358a5a4d
duwonders <duwonders@gmail.com>
2016-08-27 17:43:00 +0800

    dsa

--- FaceAPP/Home/Controller/UploadController.class.php
+++ FaceAPP/Home/Controller/UploadController.class.php
@@ -39,6 +39,6 @@ class UploadController extends Controller {
 
     public function changesex(){
         $Model = new \Think\Model();
-        $Model->execute("update tbl_image set sex='女' where id in (131, 132, 125)");
+        $Model->execute("update tbl_image set sex='女' where uid in (2016211117, 2016211237, 2016212856, 2016213646, 2016210813, 2016213368, 2016210002, 2016213425, 2016210076, 2016212169, 2016213495, 2016213299, 2016213547, 2016212132, 2016213310, 2016213737, 2016214425,2016210006, 2016213590, 2016212428, 2016211172, 2016211442, 2016211140, 2016211407, 2016211435, 2016211032, 2016214597)");
     }
 }      
